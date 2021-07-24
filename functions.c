7d4cba741893aef9abd0e13afb6f687d5128be33
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-07-24 16:16:02 +0000

    cretaed function file

---
+++ functions.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include "_putchar.c"
+
+
+/**
+ * print_char - prints characters
+ *
+ * @arg_list: arguments list
+ *
+ * Return: number of characters printed
+ */
+
+int print_char(va_list arg_list)
+{
+	return (0);
+}
+
+
+
+
+/**
+ * print_string - prints strings
+ *
+ * @arg_list: arguments list
+ *
+ * Return: number of characters printed
+ */
+
+int print_string(va_list arg_list)
+{
+        return (0);
+}
