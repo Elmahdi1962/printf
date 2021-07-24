59b7d8f2a59e9ef05daf26d4aa5f551f9945b579
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-07-24 16:09:40 +0000

    edited header file

--- holberton.h
+++ holberton.h
@@ -1,6 +1,15 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdarg.h>
+
+typedef struct format_specifiers
+{
+	char *specifier;
+	int (*printer)(va_list arg_list);
+}
 
 int _putchar(char c);
+int parser(const char* format, va_list arg_list);
+int _printf(const char *format, ...);
 
 #endif
