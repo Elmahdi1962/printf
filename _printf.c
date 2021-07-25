0ce198ba16b4939347d1d189c5fd747ae36f84b3
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2021-07-25 11:57:54 +0000

    file done

--- _printf.c
+++ _printf.c
@@ -15,14 +15,15 @@ int _printf(const char *format, ...)
 	int print_count = 0;
 	va_list arg_list;
 
-	va_start(arg_list, format);
 	if (format == NULL)
 		return (0);
 
+	va_start(arg_list, format);
 	print_count = parser(format, arg_list);
+        va_end(arg_list);
 
 	if (print_count)
 		return (print_count);
-	va_end(arg_list);
+
 	return (0);
 }
